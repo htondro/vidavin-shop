39f4805bc0c70e65dc5a5df9f9572bac8e2b04f8
htond-ro <hossein9tondro@gmail.com>
2022-06-16 10:56:35 +0430

    Initial commit

---
+++ src/pages/CategoriesPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+import { getCategories } from 'src/assets/functions';
+import { useI18n } from 'vue-i18n';
+export default defineComponent({
+  name: 'CategoriesPage',
+  setup() {
+    const categories = ref();
+    const { locale } = useI18n({ useScope: 'global' });
+    onMounted(() => {
+      getCategories(locale.value)
+        .then((res) => {
+          categories.value = res;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  },
+});
+</script>
